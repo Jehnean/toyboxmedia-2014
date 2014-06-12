ccaf6253ea7a79400e9acecaec83c44cfd2532a0
David Kern <dkern@gearkink.com>
2014-06-12 14:29:59 -0700

    Add content for header, footer, and front page

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+        <footer>
+            <span>&copy; ToyBox Media LLC</span>
+            <ul>
+                <li>1234 Eastern Ave, Las Vegas, NV 89000</li>
+                <li>+1 702 781 0072</li>
+                <li>hello@toyboxmedia.com</li>
+                <li>Twitter</li>
+                <li>Facebook</li>
+                <li>Top</li>
+            </ul>
+        </footer>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="/bootstrap/dist/js/bootstrap.min.js"></script>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,25 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>ToyboxMedia</title>
+<?php require('includes/header.php'); ?>
 
-        <link rel="stylesheet/less" type="text/css" href="/less/styles.less" />
+<section>
+    <h2>We make the digital space work for people.</h2>
+    <p>See for yourself by planning your project with our truly unique project builder.</p>
+    <button>Plan Your Digital Solution</button>
+</section>
 
-        <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
+<section>
+    <h2>What we do.</h2>
+    <p>We are a digital design agency helping businesses create strategic digital solutions
+    that look and function joyfully.</p>
+</section>
 
-        <!-- less.js is for development only -->
-        <script src="/js/less-1.7.0.min.js" type="text/javascript"></script>
-    </head>
-    <body>
-        <h1>Hello, world!</h1>
+<section>
+    <h2>Customer reviews.</h2>
+    <p>&ldquo;ToyBox Media listened to our mission, objectives values and transformed
+    them into a logo that visually represented who we were and what we were determined
+    to do in the world.  They listened to and truly cared about who we were as a company,
+    and as people, and went all in to help us make an impact.&rdquo;</p>
+    <p>-Sara Gabriella, Caramel Creative</p>
+</section>
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-        <script src="/bootstrap/dist/js/bootstrap.min.js"></script>
-    </body>
-</html>
+<section>
+    <h2>How can we help you?</h2>
+    <p>Are you interested in collaborating? We welcome your call or email so please say hello.</p>
+    <button>+1 702 781 0072</button>
+    <button>hello@toyboxmedia.com</button>
+</section>
+
+<span>Create &amp; collaborate joyfully.&trade;</span>
+
+<?php require('includes/footer.php'); ?>
